5bd82a30569df940192f11b3b333e350fd2951cc
Lucas Colantoni <lucascolantoni@hotmail.com>
2024-06-14 18:21:51 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 import CONFIG from './gitprofile.config';
 
+console.log('Themes from CONFIG:', CONFIG.themeConfig.themes);
+console.log('Custom Theme from CONFIG:', CONFIG.themeConfig.customTheme);
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
